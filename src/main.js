f93c49bef80a90a382920a335348bcd2d48c162d
Khaleb O'Brien <ebukauche52@gmail.com>
2020-10-02 02:50:00 +0100

    vue sweetalert2

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueSweetalert2);
 
 export const bus = new Vue();
 
